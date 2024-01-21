41dddae1778a3bfe555042de831b23623fa8d534
1Bc <mycodeisx@gmail.com>
2024-01-21 14:04:25 +0100

    add MQTT to docker compose

--- src/index.ts
+++ src/index.ts
@@ -128,3 +128,37 @@ myDataSource
     .catch((err) => {
         console.error("Error during Data Source initialization:", err)
     });
+
+const host = 'localhost'
+const port = '1883'
+const clientId = `mqtt_${Math.random().toString(16).slice(3)}`
+
+const connectUrl = `mqtt://${host}:${port}`
+
+const client = mqtt.connect(connectUrl, {
+    clientId,
+    clean: true,
+    connectTimeout: 4000,
+    username: 'username',
+    password: 'G516cD8#rSb£',
+    reconnectPeriod: 1000,
+})
+
+const topic = '/nodejs/mqtt'
+
+client.on('connect', () => {
+    console.log('Connected')
+
+    client.subscribe([topic], () => {
+        console.log(`Subscribe to topic '${topic}'`)
+        client.publish(topic, 'nodejs mqtt test', { qos: 0, retain: false }, (error) => {
+            if (error) {
+                console.error(error)
+            }
+        })
+    })
+})
+
+client.on('message', (topic, payload) => {
+    console.log('Received Message:', topic, payload.toString())
+})
